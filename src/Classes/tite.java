a3cd57bf3cf604ffd60de194aeee80531d35ceab
tonytoes <anthonymiguel2005@gmail.com>
2025-03-29 17:19:37 +0800

    commit

---
+++ src/Classes/tite.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import Frames.Main;
+
+/**
+ *
+ * @author Anthony
+ */
+public class tite {
+    public static void main(String args[]) {
+         
+        System.out.println("Meme");
+        UIcolors color = new UIcolors();
+        color.UIcolor();
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+            
+            
+        });
+    }
+}
