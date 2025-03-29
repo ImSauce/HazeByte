9587b692e9390790abd2b8d6110f9997f0e53f43
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-03-29 17:21:17 +0800

    NIGGA

--- src/Classes/tite.java
+++ src/Classes/tite.java
@@ -14,6 +14,7 @@ public class tite {
     public static void main(String args[]) {
          
         System.out.println("Meme");
+        System.out.println("DFIHDASIOFHSDAUFGSDIFGADIPFAD");
         UIcolors color = new UIcolors();
         color.UIcolor();
        
