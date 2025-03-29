d240e7597485251e35cf00f80b1d35b72e88a208
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-03-29 16:51:30 +0800

    asdasd latesty

--- src/Frames/Main.java
+++ src/Frames/Main.java
@@ -20,30 +20,34 @@ public class Main extends javax.swing.JFrame {
         History_BT = new javax.swing.JLabel();
         layers = new javax.swing.JLayeredPane();
         Option = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         Home = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         History = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(1300, 720));
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
-        menu.setBackground(new java.awt.Color(204, 0, 204));
+        menu.setBackground(new java.awt.Color(24, 23, 23));
+        menu.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(51, 51, 51), 1, true));
 
-        Home_BT.setText("icon");
+        Home_BT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/home.png"))); // NOI18N
         Home_BT.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 Home_BTMouseClicked(evt);
             }
         });
 
-        Option_BT.setText("icon");
+        Option_BT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/customize.png"))); // NOI18N
         Option_BT.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 Option_BTMouseClicked(evt);
             }
         });
 
-        History_BT.setText("icon");
+        History_BT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/history.png"))); // NOI18N
         History_BT.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 History_BTMouseClicked(evt);
@@ -55,70 +59,99 @@ public class Main extends javax.swing.JFrame {
         menuLayout.setHorizontalGroup(
             menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(menuLayout.createSequentialGroup()
-                .addGap(33, 33, 33)
+                .addContainerGap()
                 .addGroup(menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(History_BT)
                     .addComponent(Option_BT)
                     .addComponent(Home_BT))
-                .addContainerGap(101, Short.MAX_VALUE))
+                .addContainerGap(8, Short.MAX_VALUE))
         );
         menuLayout.setVerticalGroup(
             menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(menuLayout.createSequentialGroup()
-                .addGap(48, 48, 48)
+                .addGap(60, 60, 60)
                 .addComponent(Home_BT)
-                .addGap(45, 45, 45)
+                .addGap(30, 30, 30)
                 .addComponent(Option_BT)
-                .addGap(54, 54, 54)
+                .addGap(30, 30, 30)
                 .addComponent(History_BT)
-                .addContainerGap(185, Short.MAX_VALUE))
+                .addContainerGap(372, Short.MAX_VALUE))
         );
 
         getContentPane().add(menu);
 
         layers.setLayout(new java.awt.CardLayout());
 
-        Option.setBackground(new java.awt.Color(153, 0, 204));
+        Option.setBackground(new java.awt.Color(24, 23, 23));
+        Option.setPreferredSize(new java.awt.Dimension(783, 594));
+
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("add edit");
 
         javax.swing.GroupLayout OptionLayout = new javax.swing.GroupLayout(Option);
         Option.setLayout(OptionLayout);
         OptionLayout.setHorizontalGroup(
             OptionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 474, Short.MAX_VALUE)
+            .addGroup(OptionLayout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(jLabel1)
+                .addContainerGap(709, Short.MAX_VALUE))
         );
         OptionLayout.setVerticalGroup(
             OptionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(OptionLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addComponent(jLabel1)
+                .addContainerGap(561, Short.MAX_VALUE))
         );
 
         layers.add(Option, "card3");
 
-        Home.setBackground(new java.awt.Color(255, 255, 255));
+        Home.setBackground(new java.awt.Color(24, 23, 23));
+        Home.setPreferredSize(new java.awt.Dimension(783, 594));
+
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("games");
 
         javax.swing.GroupLayout HomeLayout = new javax.swing.GroupLayout(Home);
         Home.setLayout(HomeLayout);
         HomeLayout.setHorizontalGroup(
             HomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 474, Short.MAX_VALUE)
+            .addGroup(HomeLayout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(jLabel2)
+                .addContainerGap(698, Short.MAX_VALUE))
         );
         HomeLayout.setVerticalGroup(
             HomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(HomeLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel2)
+                .addContainerGap(557, Short.MAX_VALUE))
         );
 
         layers.add(Home, "card2");
 
-        History.setBackground(new java.awt.Color(255, 51, 51));
+        History.setBackground(new java.awt.Color(24, 23, 23));
+
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("transaction history");
 
         javax.swing.GroupLayout HistoryLayout = new javax.swing.GroupLayout(History);
         History.setLayout(HistoryLayout);
         HistoryLayout.setHorizontalGroup(
             HistoryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 474, Short.MAX_VALUE)
+            .addGroup(HistoryLayout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel3)
+                .addContainerGap(650, Short.MAX_VALUE))
         );
         HistoryLayout.setVerticalGroup(
             HistoryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(HistoryLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addComponent(jLabel3)
+                .addContainerGap(561, Short.MAX_VALUE))
         );
 
         layers.add(History, "card4");
@@ -168,6 +201,9 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel Home_BT;
     private javax.swing.JPanel Option;
     private javax.swing.JLabel Option_BT;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollBar jScrollBar1;
     private javax.swing.JLayeredPane layers;
     private javax.swing.JPanel menu;
