d9f94b7df2b42025ba43f2ade6ee29c5accc5d16
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-03-29 17:06:24 +0800

    coommit tonny request

--- src/Frames/Main.java
+++ src/Frames/Main.java
@@ -6,7 +6,8 @@ public class Main extends javax.swing.JFrame {
 
     public Main() {
         initComponents();
-        Home.setVisible(true);
+        Home_BTMouseClicked(null);
+      
     }
 
     @SuppressWarnings("unchecked")
@@ -18,7 +19,9 @@ public class Main extends javax.swing.JFrame {
         Home_BT = new javax.swing.JLabel();
         Option_BT = new javax.swing.JLabel();
         History_BT = new javax.swing.JLabel();
+        Info_BT = new javax.swing.JLabel();
         layers = new javax.swing.JLayeredPane();
+        Info = new javax.swing.JPanel();
         Option = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         Home = new javax.swing.JPanel();
@@ -54,6 +57,13 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        Info_BT.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/info.png"))); // NOI18N
+        Info_BT.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                Info_BTMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout menuLayout = new javax.swing.GroupLayout(menu);
         menu.setLayout(menuLayout);
         menuLayout.setHorizontalGroup(
@@ -63,8 +73,9 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(History_BT)
                     .addComponent(Option_BT)
-                    .addComponent(Home_BT))
-                .addContainerGap(8, Short.MAX_VALUE))
+                    .addComponent(Home_BT)
+                    .addComponent(Info_BT))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         menuLayout.setVerticalGroup(
             menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -75,12 +86,27 @@ public class Main extends javax.swing.JFrame {
                 .addComponent(Option_BT)
                 .addGap(30, 30, 30)
                 .addComponent(History_BT)
-                .addContainerGap(372, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
+                .addComponent(Info_BT)
+                .addGap(14, 14, 14))
         );
 
         getContentPane().add(menu);
 
         layers.setLayout(new java.awt.CardLayout());
+
+        javax.swing.GroupLayout InfoLayout = new javax.swing.GroupLayout(Info);
+        Info.setLayout(InfoLayout);
+        InfoLayout.setHorizontalGroup(
+            InfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 443, Short.MAX_VALUE)
+        );
+        InfoLayout.setVerticalGroup(
+            InfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 380, Short.MAX_VALUE)
+        );
+
+        layers.add(Info, "card5");
 
         Option.setBackground(new java.awt.Color(24, 23, 23));
         Option.setPreferredSize(new java.awt.Dimension(783, 594));
@@ -95,14 +121,14 @@ public class Main extends javax.swing.JFrame {
             .addGroup(OptionLayout.createSequentialGroup()
                 .addGap(31, 31, 31)
                 .addComponent(jLabel1)
-                .addContainerGap(709, Short.MAX_VALUE))
+                .addContainerGap(369, Short.MAX_VALUE))
         );
         OptionLayout.setVerticalGroup(
             OptionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(OptionLayout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addComponent(jLabel1)
-                .addContainerGap(561, Short.MAX_VALUE))
+                .addContainerGap(347, Short.MAX_VALUE))
         );
 
         layers.add(Option, "card3");
@@ -120,14 +146,14 @@ public class Main extends javax.swing.JFrame {
             .addGroup(HomeLayout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addComponent(jLabel2)
-                .addContainerGap(698, Short.MAX_VALUE))
+                .addContainerGap(358, Short.MAX_VALUE))
         );
         HomeLayout.setVerticalGroup(
             HomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(HomeLayout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addComponent(jLabel2)
-                .addContainerGap(557, Short.MAX_VALUE))
+                .addContainerGap(343, Short.MAX_VALUE))
         );
 
         layers.add(Home, "card2");
@@ -144,14 +170,14 @@ public class Main extends javax.swing.JFrame {
             .addGroup(HistoryLayout.createSequentialGroup()
                 .addGap(35, 35, 35)
                 .addComponent(jLabel3)
-                .addContainerGap(650, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         HistoryLayout.setVerticalGroup(
             HistoryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(HistoryLayout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addComponent(jLabel3)
-                .addContainerGap(561, Short.MAX_VALUE))
+                .addContainerGap(347, Short.MAX_VALUE))
         );
 
         layers.add(History, "card4");
@@ -168,19 +194,29 @@ public class Main extends javax.swing.JFrame {
         Home.setVisible(true);
         Option.setVisible(false);
         History.setVisible(false);
+        Info.setVisible(false);
     }//GEN-LAST:event_Home_BTMouseClicked
 
     private void Option_BTMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Option_BTMouseClicked
         Home.setVisible(false);
         Option.setVisible(true);
         History.setVisible(false);
+        Info.setVisible(false);
     }//GEN-LAST:event_Option_BTMouseClicked
 
     private void History_BTMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_History_BTMouseClicked
         Home.setVisible(false);
         Option.setVisible(false);
         History.setVisible(true);
+        Info.setVisible(false);
     }//GEN-LAST:event_History_BTMouseClicked
+
+    private void Info_BTMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Info_BTMouseClicked
+        Home.setVisible(false);
+        Option.setVisible(false);
+        History.setVisible(false);
+        Info.setVisible(true);
+    }//GEN-LAST:event_Info_BTMouseClicked
     //----------------MENU BUTTONS -------------//
     
     
@@ -199,6 +235,8 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel History_BT;
     private javax.swing.JPanel Home;
     private javax.swing.JLabel Home_BT;
+    private javax.swing.JPanel Info;
+    private javax.swing.JLabel Info_BT;
     private javax.swing.JPanel Option;
     private javax.swing.JLabel Option_BT;
     private javax.swing.JLabel jLabel1;
